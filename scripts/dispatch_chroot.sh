69279ddfabdaa67ff9d1e64bc83c49fec81172f3
Vitaly Grinin <grivital@yandex.ru>
2026-04-06 19:53:11 +0300

    Revert "fix: temporarily disable set -u when sourcing /etc/profile"
    This reverts commit 9b71e27f222657dd7dc21ff556d18569b681df33.

--- scripts/dispatch_chroot.sh
+++ scripts/dispatch_chroot.sh
@@ -4,12 +4,8 @@ set -uo pipefail
 [[ $EXECUTED_IN_CHROOT != "true" ]] \
 	&& { echo "This script must not be executed directly!" >&2; exit 1; }
 
-# Temporarily disable unbound variable check to avoid issues with profile.d scripts
-# that may reference undefined variables (e.g., debuginfod.sh with DEBUGINFOD_URLS)
-set +u
 # Source the systems profile
 source /etc/profile
-set -u
 
 # Set safe umask
 umask 0077
